977a91250cadf4189cba1666fe4c644c55b1b0fe
KartikFularaTCZ <kartik.fulara@techchefz.com>
2024-05-10 10:31:38 +0530

    adding server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+import cors from "cors";
+import morgon from "morgan";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,13 +11,41 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = 6969;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+app.use(cors());
+app.use(morgon("dev"));
+
+// Function to read JSON data from file
+const readJsonFromFile = (filePath) => {
+  try {
+    const jsonData = fs.readFileSync(filePath);
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error("Error reading file:", error);
+    return null;
+  }
+};
+
+// Route to get filtered data from JSON file
+app.get("/db", (req, res) => {
+  const { type } = req.query; // Assuming params are passed as query parameters
+  const relativePath = path.join(__dirname, "db.json");
+  const jsonData = readJsonFromFile(relativePath);
+
+  if (!jsonData) {
+    res.status(500).send("Error reading JSON data from file.");
+    return;
+  }
+  if (type) {
+    const data = jsonData[type];
+    res.json(data);
+    return;
+  }
+  console.log(jsonData);
+  res.json(jsonData);
 });
 
-app.get("/db", (req, res) => {
-  const relativePath = path.join(__dirname, "db.json");
-  res.sendFile(relativePath); // Corrected to use res.sendFile
+app.all("*", (req, res) => {
+  res.send(JSON.stringify({ message: "Something went wrong" }));
 });
 
 app.listen(PORT, () => {
